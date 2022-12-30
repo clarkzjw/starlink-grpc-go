41f095c15219b2403a456f9e7980b677c6a2e279
clarkzjw <i@jinwei.me>
2022-12-30 15:03:13 -0800

    update dish.protoset from 192.168.1.1:9000

--- build/get-protoc.sh
+++ build/get-protoc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # requires https://github.com/fullstorydev/grpcurl
-grpcurl -plaintext -protoset-out proto/dish.protoset 192.168.100.1:9200 describe SpaceX.API.Device.Device
+#grpcurl -plaintext -protoset-out proto/dish.protoset 192.168.100.1:9200 describe SpaceX.API.Device.Device
+grpcurl -plaintext -protoset-out proto/dish.protoset 192.168.1.1:9000 describe SpaceX.API.Device.Device
